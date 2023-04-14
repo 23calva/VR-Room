1c3a4dc57fea2684110e2a26cf6e15b36ba9b2be
Adam <66341882+LeftoverAtoms@users.noreply.github.com>
2023-04-14 01:25:04 -0400

    It works now

--- Assets/Scripts/Socket.cs
+++ Assets/Scripts/Socket.cs
@@ -7,145 +7,181 @@ using UnityEngine.XR.Interaction.Toolkit;
 /// </summary>
 public class Socket : MonoBehaviour
 {
-    private static XRRayInteractor leftController;
-    private static XRRayInteractor rightController;
-
     /// <summary>
-    /// References to the GameObject and Rigidbody Component within this socket.
+    /// References for the controllers
     /// </summary>
-    private GameObject obj;
+    public static XRRayInteractor rightController, leftController;
 
     /// <summary>
-    /// Collider radius.
+    /// References for the game object and important components within this socket
     /// </summary>
-    public float size = 0.5f;
+    private SocketChild child = new SocketChild();
 
     /// <summary>
-    /// Speed at which the socketed entity travels.
+    /// Speed at which an object travels to be socketed
     /// </summary>
     public float lerpSpeed = 5.0f;
 
     /// <summary>
-    /// Tolerance.
+    /// Socket radius
     /// </summary>
-    public float MaxAngle = 90.0f;
+    public float size = 0.5f;
+
+    /// <summary>
+    /// Maximum angle considered valid for an object to be socketed
+    /// </summary>
+    public float tolerance = 90.0f;
 
     private void Start()
     {
         // Assign controllers if they are not yet defined.
-        leftController ??= GameObject.Find("LeftHand Controller").GetComponent<XRRayInteractor>();
-        rightController ??= GameObject.Find("RightHand Controller").GetComponent<XRRayInteractor>();
+        rightController ??= GameObject.Find("LeftHand Controller").GetComponent<XRRayInteractor>();
+        leftController ??= GameObject.Find("RightHand Controller").GetComponent<XRRayInteractor>();
 
+        // Add the trigger component and set values
         var collider = this.gameObject.AddComponent<SphereCollider>();
+        collider.hideFlags = HideFlags.HideInInspector;
         collider.isTrigger = true;
         collider.radius = size;
     }
 
     private void Update()
     {
-        print(leftController.attachTransform.name);
-
-        if (leftController.attachTransform.gameObject == obj || 
-            rightController.attachTransform.gameObject == obj)
+        // Grabbing the child object from the socket would
+        // not reset the color or unparent properly
+        if(this.transform.childCount > 0)
         {
+            this.transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.white;
+            this.transform.DetachChildren();
+        }
+
+        if(child.gameObject == null)
+            return;
+
+        if(IsGrabbed(child.gameObject))
+        {
+            RemoveChild();
             return;
         }
 
-        if (obj != null)
+        // Leap child to the center of the socket
+        float distance = Vector3.Distance(child.gameObject.transform.position, this.transform.position);
+        if(distance <= 0.001f)
         {
-            // Lerp til you can't no more.
-            float distance = Vector3.Distance(obj.transform.position, this.transform.position);
-            if (distance <= 0.001f)
-            {
-                obj.transform.position = this.transform.position;
-            }
-            else
-            {
-                obj.transform.position = Vector3.Lerp(obj.transform.position, this.transform.position, lerpSpeed * Time.deltaTime);
-            }
+            child.gameObject.transform.position = this.transform.position;
+        }
+        else
+        {
+            child.gameObject.transform.position = Vector3.Lerp(child.gameObject.transform.position, this.transform.position, lerpSpeed * Time.deltaTime);
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        var entity = other.gameObject;
-
-        // Socket is occupied.
-        if (obj != null)
-        {
+        // Socket is occupied
+        if(child.gameObject != null)
             return;
-        }
 
-        // Modify render color.
-        var renderer = entity.GetComponent<MeshRenderer>();
-        if (IsValidAngle(entity))
+        // References
+        var obj = other.gameObject;
+        var renderer = obj.GetComponent<MeshRenderer>();
+
+        // Modify colors based on object rotation
+        if(IsValidAngle(obj))
         {
             renderer.material.color = Color.green;
+
+            // Socket the object in this trigger
+            if(IsGrabbed(obj) == false)
+            {
+                child.gameObject = obj;
+                child.gameObject.transform.SetParent(this.transform);
+                child.renderer = renderer;
+                child.Color = Color.white;
+
+                // Preserve horizontal rotation
+                //child.gameObject.transform.rotation = Quaternion.Euler(0, child.gameObject.transform.rotation.eulerAngles.y, 0);
+            }
         }
         else
         {
             renderer.material.color = Color.red;
-            return;
         }
-
-        InsertObject(entity);
     }
 
-    /// <summary>
-    /// Remove the socketed object when it has exited this trigger.
-    /// </summary>
+#if UNITY_EDITOR
+
+    // NOTE: This doesn't actually do anything in VR
     private void OnTriggerExit(Collider collider)
     {
         Vector3 point = collider.ClosestPoint(this.transform.position);
         float distance = Vector3.Distance(point, this.transform.position);
 
-        if (collider.gameObject == obj && distance > size)
+        if(collider.gameObject == child.gameObject && distance > size)
         {
-            RemoveObject();
+            RemoveChild();
         }
+    }
+
+#endif
+
+    /// <summary>
+    /// Does the user have this object in their hands?
+    /// </summary>
+    private bool IsGrabbed(GameObject obj)
+    {
+        //
+        // This code is a bit of a hack but probably
+        // one of the easiest ways to get what we want
+        //
+
+        if(rightController.hasSelection)
+        {
+            if(rightController.interactablesSelected[0].transform.gameObject == obj)
+            {
+                return true;
+            }
+        }
+        if(leftController.hasSelection)
+        {
+            if(leftController.interactablesSelected[0].transform.gameObject == obj)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     /// <summary>
     /// Does this object align with this socket?
     /// </summary>
-    public bool IsValidAngle(GameObject socketedObject)
+    public bool IsValidAngle(GameObject obj)
     {
-        float angle = Quaternion.Angle(socketedObject.transform.rotation, this.transform.rotation);
+        //
+        // It would be incredibly easy to set tolerances for each axis
+        //
+
+        float angle = Quaternion.Angle(obj.transform.rotation, this.transform.rotation);
         angle = Math.Abs(angle);
-        return angle <= MaxAngle;
+        return angle <= tolerance;
     }
 
     /// <summary>
-    /// Manage this object with this function.
+    /// Remove the child object
     /// </summary>
-    private void InsertObject(GameObject gameObject)
+    private void RemoveChild()
     {
-        obj = gameObject;
-        obj.transform.SetParent(this.transform);
-    }
-
-    private void RemoveObject()
-    {
-        //Vector3 parentPos = obj.transform.localPosition + this.transform.position;
-        //Quaternion parentRot = obj.transform.localRotation * this.transform.rotation;
-
-        obj.GetComponent<MeshRenderer>().material.color = Color.white;
-
-        this.transform.DetachChildren();
-
-        //obj.transform.position = parentPos;
-        //obj.transform.rotation = parentRot;
-
-        obj = null;
+        child.gameObject = null;
+        child.renderer = null;
     }
 
     private void OnDrawGizmos()
     {
-        // Draw a sphere.
+        // Draw a sphere
         Gizmos.color = Color.black;
         Gizmos.DrawWireSphere(this.transform.position, size);
 
-        // Draw an arrow.
+        // Draw an arrow
         Gizmos.color = Color.red;
         float length = 0.75f;
         float arms = length / 4;
@@ -153,5 +189,18 @@ public class Socket : MonoBehaviour
         Gizmos.DrawLine(this.transform.position, end);
         Gizmos.DrawLine(end, (this.transform.right - this.transform.forward) * arms + end);
         Gizmos.DrawLine(end, (-this.transform.right - this.transform.forward) * arms + end);
+    }
+}
+
+[Serializable]
+internal class SocketChild
+{
+    public GameObject gameObject = null;
+    public MeshRenderer renderer = null;
+
+    public Color Color
+    {
+        get => renderer.material.color;
+        set => renderer.material.color = value;
     }
 }
